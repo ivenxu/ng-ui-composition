1d4e8f908e911eaa58be5b536323650f38148e81
Iven <ivenxu@gmail.com>
2017-12-27 20:59:00 +1100

    add top bar for care

--- src/care/care.module.ts
+++ src/care/care.module.ts
@@ -1,13 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-// import { NoopAnimationsModule  } from '@angular/platform-browser/animations';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { CustomerSummaryComponent } from './customersummary/customer-summary.component';
 import { BillingModule } from '../billing/billing.module';
 import { ShareModule } from '../share/share.module';
 import { CareAppComponent } from './app/care-app.component';
+import { TopBarComponent } from './topbar/top-bar.component';
 import { NgxChartsModule } from '@swimlane/ngx-charts';
 
 
@@ -19,8 +19,8 @@ import { NgxChartsModule } from '@swimlane/ngx-charts';
       NgxChartsModule,
       BillingModule,
       ShareModule],
-    declarations: [  CareAppComponent, DashboardComponent, CustomerSummaryComponent  ],
-    exports: [ CareAppComponent, DashboardComponent, CustomerSummaryComponent ],
+    declarations: [  CareAppComponent, DashboardComponent, CustomerSummaryComponent, TopBarComponent ],
+    exports: [ CareAppComponent, DashboardComponent, CustomerSummaryComponent, TopBarComponent ],
     providers: [ ],
     bootstrap: [ CareAppComponent ]
   })
---
+++ src/care/topbar/top-bar.component.ts
@@ -0,0 +1,9 @@
+import { Component }      from '@angular/core';
+
+@Component({
+    selector: 'top-bar-selector',
+    templateUrl: 'care/topbar/top-bar.component.html'
+  })
+  export class TopBarComponent {
+    title = 'top bar';
+  }
